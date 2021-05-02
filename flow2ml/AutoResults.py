1a9978c29d8ddcf1091aed45afe37c5518960723
ArkaprabhaChakraborty <chakrabortyarkaprabha998@gmail.com>
2021-05-02 22:17:35 +0530

    Updates

--- flow2ml/AutoResults.py
+++ flow2ml/AutoResults.py
@@ -7,43 +7,68 @@ from sklearn.metrics import plot_confusion_matrix
 from sklearn.metrics import plot_precision_recall_curve
 from docx import Document
 from docx.shared import Inches
+import os
+
 
 class Auto_Results:
     def __init__(self,model,X_test,Y_test):
-        self.model = model
-        self.x_test = X_test
-        self.prediction = self.model.predict_proba(X_test)
-        self.y_test = Y_test
-    
+        '''
+        Initialises the class variables.
+        '''
+        self.model = model          #stores the model
+        self.x_test = X_test        #stores the X_test (test data) 
+        self.prediction = self.model.predict_proba(X_test)     #stores the probabilities for roc
+        self.y_test = Y_test    #stores the Y_test (labels)
+        try:                        #try creating a Results directory and show excpetion if it already exists 
+            p = os.getcwd()
+            p = os.path.join(p,"Results")
+            self.results_path = p
+            os.mkdir(self.results_path)
+        except:
+            print("Results Folder already exists")
+
     def roc_curve(self,figure_name="roc.jpeg"):
+        '''
+        Plotting the Roc curve for the Input model
+        '''
         i= self.prediction.shape
         try:
             if (i[1]>1):
                 self.prediction = self.prediction[:, 1]
         except:
-            pass
-        auc_lr = roc_auc_score(self.y_test, self.prediction)
+            print("Predictions vector doesn't need slicing")
+        auc_lr = roc_auc_score(self.y_test, self.prediction)  
         fpr_lr, tpr_lr, thresholds_lr = roc_curve(self.y_test, self.prediction)
         plt.figure(figsize=(50, 20))
         plt.plot(fpr_lr, tpr_lr)
         plt.title('ROC Curve', fontsize=70)
         plt.xlabel('False Positive Rate', fontsize=65)
         plt.ylabel('True Positive Rate', fontsize=65)
-        plt.savefig(figure_name)
+        plt.savefig(self.results_path+'/'+figure_name)
     
     def confusion_matrix(self):
+        '''
+        Plotting the confusion matrix
+        '''
         plt.figure(figsize=(50,20))
         plot_confusion_matrix(self.model,self.x_test,self.y_test)
         plt.title('Confusion Matrix', size = 35)
-        plt.savefig('confusion_matrix.jpeg')
+        plt.savefig(self.results_path+'/confusion_matrix.jpeg')
     
     def precision_recall_curve(self,figure_name="prc.jpeg"):
+        '''
+        Plotting the precision recall curve 
+        '''
         plt.figure(figsize=(50,20))
         plot_precision_recall_curve(self.model,self.x_test,self.y_test)
         plt.title('Precision Recall Curve', size = 35)
-        plt.savefig(figure_name)
+        plt.savefig(self.results_path+'/'+figure_name)
 
-    def get_results_pdf(self,file_name="report.docx"):
+    def get_results_pdf(self,file_name="report"):
+        '''
+        Saves all the plots with their default name and 
+        creates a report.docx file in the Results Folder
+        '''
         self.roc_curve()
         self.confusion_matrix()
         self.precision_recall_curve()
@@ -58,7 +83,7 @@ This curve plots two parameters:
 An ROC curve plots TPR vs. FPR at different classification thresholds. Lowering the classification threshold classifies more items as positive, thus increasing both False Positives and True Positives.
 The ROC curve for the Input Model is As Follows.
 ''')
-        doc.add_picture('roc.jpeg',width=Inches(6.0))
+        doc.add_picture(self.results_path+'/roc.jpeg',width=Inches(6.0))
         doc.add_paragraph('''  ''')
         doc.add_heading('Confusion Matrix', level=2)
         doc.add_paragraph('''
@@ -69,7 +94,7 @@ The name stems from the fact that it makes it easy to see whether the system is 
 It is a special kind of contingency table, with two dimensions ("actual" and "predicted"), and identical sets of "classes" in both dimensions (each combination of dimension and class is a variable in the contingency table).
 The Confusion Matrix for the Input Model is As shown:       
 ''')
-        doc.add_picture('confusion_matrix.jpeg',width=Inches(5.0))
+        doc.add_picture(self.results_path+'/confusion_matrix.jpeg',width=Inches(5.0))
         doc.add_paragraph('''  ''')
         doc.add_heading("Precision Recall Curve")
         doc.add_paragraph('''
@@ -79,28 +104,7 @@ A precision-recall curve is a plot of the precision (y-axis) and the recall (x-a
 The Precision Recall Curve for the Input Model is a s shown:
 
         ''')
-        doc.add_picture('prc.jpeg',width=Inches(5.0))
-        doc.save(file_name)
-#testing codes
-'''
-df = pd.read_csv('winequality-white.csv', sep=';')
-df['quality'] = ['Good' if quality >= 7 else 'Bad' for quality in df['quality']]
+        doc.add_picture(self.results_path+'/prc.jpeg',width=Inches(5.0))
+        doc.save(self.results_path+"/"+file_name+".docx")
 
-from sklearn.model_selection import train_test_split
 
-X = df.drop('quality', axis=1)
-y = df['quality']
-
-X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.25, random_state=42
-)
-
-from sklearn.linear_model import LogisticRegression
-
-y_test_int = y_test.replace({'Good': 1, 'Bad': 0})
-y_train_int = y_train.replace({'Good': 1,"Bad": 0})
-print(y_test_int)
-model_lr = LogisticRegression(max_iter=5000).fit(X_train, y_train_int)
-auto_res = Auto_Results(model_lr,X_test,y_test_int)
-auto_res.get_results_pdf()
-'''
