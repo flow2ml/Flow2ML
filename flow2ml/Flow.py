90abd3703b7ee110e7b26a1d287f11d238bc6812
rubyruins <soumya.parekh@somaiya.edu>
2021-05-08 23:30:20 +0530

    added shear function

--- flow2ml/Flow.py
+++ flow2ml/Flow.py
@@ -113,6 +113,9 @@ class Flow(Data_Loader,Filters,Data_Augumentation):
 
         if operation == "rotate":
           self.applyRotate(path)
+        
+        if operation == "shear":
+          self.applyShear(path)
       
         time.sleep(0.1)
         self.update_progress( progress[progress_i]/100.0, f"Augmented all images in {folder}" )
