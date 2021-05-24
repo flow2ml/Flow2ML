d7f0e440b1e2de7c8a0bd50f4cf06ce7310b4295
Chebrolu Bala Sai Harika <34155580+chebroluharika@users.noreply.github.com>
2021-05-24 10:52:48 +0530

    Update model.py

--- flow2ml/models/model.py
+++ flow2ml/models/model.py
@@ -4,15 +4,19 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import GridSearchCV
 import os
 
-class ApplyModels:
+class TrainModels:
 
-    def __init__(self,algorithms) :
+    def __init__(self,algorithms, train_x, train_y, val_x, val_y) :
         self.algorithms = algorithms
+        self.train_x = train_x
+        self.train_y = train_y
+        self.val_x = val_x
+        self.val_y = val_y
 
-    def applyLogisticRegression(self,model):
-        '''Applies Logistic Regression to the model with the given activation function and metrics and returns the score with best hypertuned params.'''
+    def applyLogisticRegression(self):
+        '''Applies Logistic Regression  with the given metrics and returns the score with best hypertuned params.'''
 
-        logmodel=LogisticRegression(model,self.algorithms['logistic_regression']['activation_function'],self.algorithms['logistic_regression']['metrics'])
+        logmodel=LogisticRegression()
         # defining hyperparameters to be used for best tuning
         params_grid=[{'penalty' : ['none', 'l1', 'l2', 'elasticnet'] ,
         'C' : np.logspace(-4,4,20),
@@ -21,9 +25,11 @@ class ApplyModels:
         }
         ]
         # Apply GridSearchCV() to find the best params.
-        grided=GridSearchCV(logmodel,param_grid=params_grid, cv=3, verbose=True, n_jobs=-1)
-        #Build model using best params on test_data
-        best_fit=grided.fit(train_data,train_labels)
-        best_fit.best_estimator_
-        # return the score 
-        return best_fit.score(train_data,train_labels)
+        model_logistic_regression=GridSearchCV(logmodel,param_grid=params_grid, cv=5, verbose=True, n_jobs=-1, scoring=self.algorithms['logistic_regression']['metrics'])
+        model_logistic_regression.fit(self.train_x, self.train_y)
+        
+        print(f'Best parameters {model_logistic_regression.best_params_}')
+        print( f'Mean cross-validated ' + f'{self.algorithms['logistic_regression']['metrics']}' + score of the best_estimator: ' + f'{model_logistic_regression.best_score_:.3f}'
+              
+        # return the model
+        return model_logistic_regression
