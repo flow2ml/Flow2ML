388a88307f46eca4c302676908e93b734830956e
ArkaprabhaChakraborty <chakrabortyarkaprabha998@gmail.com>
2021-06-02 19:07:29 +0530

    init changes

--- flow2ml/__init__.py
+++ flow2ml/__init__.py
@@ -2,3 +2,4 @@ from .Flow import Flow
 from .Auto_Results import Auto_Results
 from .Tf_Results import Tf_Results
 from .Process_Csv import Process_Csv
+from .Tf_Results_without_gnerator import Tf_Results_without_generator
